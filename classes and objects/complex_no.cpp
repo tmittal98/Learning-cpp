14930729e60d87b81f9ed718af5698636e5cb10d
tusharmittal18 <tusharmittal429@gmail.com>
2019-09-19 22:24:05 +0530

    added addtion of two complex no functionality

--- classes and objects/complex_no.cpp
+++ classes and objects/complex_no.cpp
@@ -11,13 +11,22 @@ public:
     void display_data(){
         cout<<"\na = "<<a<<"\nb = "<<b<<endl;
     }
+    complex add (complex c)
+    {
+        complex temp;
+        temp.a = a + c.a;
+        temp.b = b + c.b;
+        return temp;
+    }
 };
 void complex:: set_data(int x, int y){
     a = x; b =y;
 }
 int main(){
-    complex c1;
+    complex c1,c2,c3;
     c1.set_data(3,4);
-    c1.display_data();
+    c2.set_data(5,6);
+    c3 = c1.add(c2);
+    c3.display_data();
 return 0;
 }
