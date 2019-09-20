a08c75bc680c51e87284dfbf13304e19c5c99661
tusharmittal18 <tusharmittal429@gmail.com>
2019-09-20 20:55:41 +0530

    Basic.cpp

---
+++ Constructors/Basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Car{
+//private:
+  //  char car-no[10];
+public:
+    Car(){
+    cout<<"\nHello you are in constructor :-)";
+    }
+};
+int main(){
+    Car c1,c2,c3,c4,c5;
+return 0;
+}
